2ba631ba265f5bc731bd4838371926f9e9ccfe9f
md jahirul huq <contact@shetue.net>
2025-10-28 10:23:45 +0000

    const firebaseConfig = {   apiKey: "AIzaSyBSvFAyZJJ_oP4ZYMpOcPZHfDDV1VVD

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,9 @@
+
 export const firebaseConfig = {
-  "projectId": "studio-9169148148-e27da",
-  "appId": "1:1030857823933:web:450abbb78d5dca9b9509d4",
-  "apiKey": "AIzaSyBSvFAyZJJ_oP4ZYMpOcPZHfDDV1VVDm84",
-  "authDomain": "studio-9169148148-e27da.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "1030857823933"
+  apiKey: "AIzaSyBSvFAyZJJ_oP4ZYMpOcPZHfDDV1VVDm84",
+  authDomain: "studio-9169148148-e27da.firebaseapp.com",
+  projectId: "studio-9169148148-e27da",
+  storageBucket: "studio-9169148148-e27da.appspot.com",
+  messagingSenderId: "1030857823933",
+  appId: "1:1030857823933:web:450abbb78d5dca9b9509d4"
 };
