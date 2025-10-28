110e7e5bc9f10103feb8764dfa647c403ac045cb
md jahirul huq <contact@shetue.net>
2025-10-28 21:15:58 +0000

    Set up a Firebase backend

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,8 @@
-
 export const firebaseConfig = {
-  apiKey: "AIzaSyBSvFAyZJJ_oP4ZYMpOcPZHfDDV1VVDm84",
-  authDomain: "studio-9169148148-e27da.firebaseapp.com",
-  projectId: "studio-9169148148-e27da",
-  storageBucket: "studio-9169148148-e27da.appspot.com",
-  messagingSenderId: "1030857823933",
-  appId: "1:1030857823933:web:450abbb78d5dca9b9509d4"
+  "projectId": "studio-9169148148-e27da",
+  "appId": "1:1030857823933:web:450abbb78d5dca9b9509d4",
+  "apiKey": "AIzaSyBSvFAyZJJ_oP4ZYMpOcPZHfDDV1VVDm84",
+  "authDomain": "studio-9169148148-e27da.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1030857823933"
 };
